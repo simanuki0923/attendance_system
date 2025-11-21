1e55c883bddfe59197d58047923ae373e3a363f7
simanuki mamoru <mamoru.simanuki74@gmail.com>
2025-11-21 21:51:46 +0900

    管理者画面勤怠一覧画面設定

--- src/resources/views/admin/list.blade.php
+++ src/resources/views/admin/list.blade.php
@@ -3,7 +3,7 @@
 @extends('layouts.app')
 
 @section('css')
-  <link rel="stylesheet" href="{{ asset('css/list.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/admin_list.css') }}">
 @endsection
 
 @section('content')
